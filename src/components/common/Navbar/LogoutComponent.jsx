deb665ece84cad6639acd9de57f555720501be02
Tade Maddonni <tadeomaddonni@gmail.com>
2023-01-20 23:13:00 -0300

    responsive tablet mediaqueries

--- src/components/common/Navbar/LogoutComponent.jsx
+++ src/components/common/Navbar/LogoutComponent.jsx
@@ -5,8 +5,8 @@ const LogoutComponent = () => {
 		<div>
 			<svg
 				xmlns="http://www.w3.org/2000/svg"
-				width="16"
-				height="16"
+				width="23"
+				height="23"
 				fill="currentColor"
 				className="bi bi-box-arrow-right"
 				viewBox="0 0 16 16"
